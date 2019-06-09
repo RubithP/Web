e2bd937e791f70decba5dc42e11044ac15cf6feb
RubithP <rubith135@gmail.com>
2019-06-08 20:47:23 -0500

    5 files modified, 4 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+import {Productos} from '../app/productos';
+import {ProductoClass} from '../app/producto-class';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Desarrollo Web';
+  listaProductos = Productos;
+  productoSeleccionado = ProductoClass;
+
+  compartir()
+  {
+    window.alert("producto compartir")
+  }
+  verDetalle(prod:ProductoClass): void
+  {
+    this.productoSeleccionado = prod;
+  }
 }
--- src/app/productos.ts
+++ src/app/productos.ts
@@ -1,2 +1,22 @@
-export class Productos {
-}
+import {ProductoClass} from '../app/producto-class'
+//export const Productos =[
+export const Productos: ProductoClass[]=[
+  {
+    nombre: "Huawei P30 PRO",
+    precio : 799,
+    descripcion: "Es un excelente Smartphone pero sin tienda de app",
+    fabricante: "Huawei Corp."
+  },
+  {
+    nombre: "Samsung Galaxy S10",
+    precio : 999,
+    descripcion: "Es un excelente Smartphone para ver fotos",
+    fabricante: "Samsung"
+  },
+  {
+    nombre: "Iphone X",
+    precio : 699,
+    descripcion: "Diseño Compacto",
+    fabricante: "Apple Inc."
+  },
+]
